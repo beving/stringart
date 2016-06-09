5aa18a6d06a83a4a9c2344f7ef6e10b949c8fe7b
Mark Bevington <beving@yahoo.com>
2016-06-09 13:51:57 -0500

    Cleanup SaveActivity

--- app/src/main/java/com/marksoft/stringart/SaveActivity.java
+++ app/src/main/java/com/marksoft/stringart/SaveActivity.java
@@ -1,12 +1,10 @@
 package com.marksoft.stringart;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -54,11 +52,6 @@ public class SaveActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-//        for (String fileFound: context.fileList()) {
-//            Log.d("SaveActivity", "File---> " +fileFound);
-//            Toast.makeText(context, "Saved as: " + fileFound, Toast.LENGTH_LONG).show();
-//        }
-
         Log.d("SaveActivity", "File-?????--> " +context.getFileStreamPath(fileName));
 
         return context.getFileStreamPath(fileName);
@@ -100,58 +93,4 @@ public class SaveActivity extends AppCompatActivity {
         Log.d("SaveActivity", "getDefaultShareIntent startActivity worked!");
         return sharingIntent;
     }
-
-//    private File saveToExternalStorage(Bitmap b, Context context){
-//        // Get path to External Storage
-//        //String root = Environment.getExternalStorageDirectory().toString();
-//
-//        File rootFile = context.getFilesDir();
-//
-//        Log.d("SaveActivity","Root Path name: " + rootFile.getPath());
-//        File myDir = new File(rootFile.getPath() + File.separator + "MarkWasHere" + File.separator);
-//        Log.d("SaveActivity","myDir: " + myDir.getAbsolutePath());
-//        myDir.mkdirs();
-//        Log.d("SaveActivity", "mkdirs ");
-//
-//        // Generating a random number to save as image name
-//        Random generator = new Random();
-//        int n = 10000;
-//        n = generator.nextInt(n);
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
-//        String fname = "Image_"+timeStamp+"_"+ n + ".jpg";
-//
-//        Log.d("SaveActivity", "fname: " + fname);
-//        File file = new File(myDir, fname);
-//        try {
-//            if (file.exists()) {
-//                file.delete();
-//            }
-//
-//            FileOutputStream out = new FileOutputStream(file);
-//            b.compress(Bitmap.CompressFormat.JPEG, 90, out);
-//            out.flush();
-//            out.close();
-//        } catch (Exception e) {
-//            Log.d("DrawingView","Exception --> File Name: " +file.getAbsolutePath());
-//            throw new RuntimeException(e);
-//        }
-//
-//        if (file.exists()) {
-//            Log.d("DrawingView","2File does exist --> File Name: " +file.getAbsolutePath());
-//            Toast.makeText(context, "File does exist --> File Name: " +file.getAbsolutePath(),  Toast.LENGTH_LONG).show();
-//        } else Log.d("SaveActivity", "2File does not exist: " + file.getAbsolutePath());
-//
-//        // Tell your media scanner to refresh/scan for new images
-//
-//        MediaScannerConnection.scanFile(context,
-//                new String[]{file.toString()}, null,
-//                new MediaScannerConnection.OnScanCompletedListener() {
-//                    public void onScanCompleted(String path, Uri uri) {
-//                        System.out.println("MediaScannerConnection OnScapath");
-//                    }
-//                });
-//        return file;
-//
-//    }
-
 }
