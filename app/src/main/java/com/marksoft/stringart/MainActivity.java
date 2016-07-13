2e502cee5261ff5f24e606ce351f83178d525413
Mark Bevington <beving@yahoo.com>
2016-07-13 09:33:50 -0500

    Share is now working, not perfect but working.

--- app/src/main/java/com/marksoft/stringart/MainActivity.java
+++ app/src/main/java/com/marksoft/stringart/MainActivity.java
@@ -1,15 +1,31 @@
 package com.marksoft.stringart;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import com.stephentuso.welcome.WelcomeScreenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -106,6 +122,17 @@ public class MainActivity extends AppCompatActivity {
 //                    ent intent = new Intent(this, GLES20Activity.class);
 //                    startActivity(intent);
                     //new ShareActivity().share(MainActivity.this, getDrawingView());
+//                    File savedFile = save( getDrawingView());
+//                    shareIntent(savedFile);
+//                    whatefver();
+
+                    requestPermissions();
+
+                    ShareActivity.hasPermission(this);
+
+                    ShareActivity.share(this, getDrawingView());
+
+
                     break;
                 }
                 default: {
@@ -116,8 +143,9 @@ public class MainActivity extends AppCompatActivity {
             getDrawingView().reDraw();
 
         } catch (Exception e) {
-            Log.e("MainActivity", "Caught Exception " + e.getMessage());
+            Log.e("", e.getStackTrace().toString());
             e.printStackTrace();
+
             Toast.makeText(MainActivity.this, "Caught Exception " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
 
@@ -159,6 +187,137 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         welcomeScreen.onSaveInstanceState(outState);
         getDataFragment().onSaveInstanceState(outState);
+    }
+
+    private void whatefver() {
+
+        File fileToShare = new File("/data/user/0/com.marksoft.stringart/files/Image_20160712_9593.png");
+
+        if (fileToShare.exists()) {
+            Toast.makeText(this, "Image_20160712_9593 exists", Toast.LENGTH_LONG).show();
+        } else   Toast.makeText(this, "NOT Image_20160712_9593  ", Toast.LENGTH_LONG).show();
+
+        Intent intent = ShareIntent.getImageIntent(Uri.fromFile(fileToShare));
+        startActivity(intent);
+    }
+
+
+    private void whatefdddfdfdfdver() {
+
+        File fileToShare = new File("/data/user/0/com.marksoft.stringart/files/Image_20160712_9593.png");
+
+        if (fileToShare.exists()) {
+            Toast.makeText(this, "Image_20160712_9593 exists", Toast.LENGTH_LONG).show();
+        } else   Toast.makeText(this, "NOT Image_20160712_9593  ", Toast.LENGTH_LONG).show();
+
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/png");
+        //final File photoFile = new File(getFilesDir(), "foo.jpg");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(fileToShare));
+        startActivity(Intent.createChooser(shareIntent, "Share image using"));
+    }
+
+
+    private File save(final DrawingView drawingView) {
+
+        // Generating a random number to save as image name
+        String fileName = getRandomFileName();
+
+        //Log.d("DrawingView", "context.getFilesDir: " + context.getFilesDir());
+
+        View content = drawingView;
+        content.setDrawingCacheEnabled(true);
+        content.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        Bitmap bitmap = content.getDrawingCache();
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);  //TODO make this use a temp file so it gets thrown away see: https://developer.android.com/training/basics/data-storage/files.html
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return getFileStreamPath(fileName);
+    }
+
+
+
+
+   private void shareIntent(File file) {
+   //private void shareIntent(Uri uri, Context context) {
+        //Bitmap icon = mBitmap;
+        try {
+
+            String debugPath = Uri.fromFile(file).getPath();
+            File debugFile = new File(debugPath);
+
+            if (!debugFile.exists()) {
+                Toast.makeText(this, "NOT exists! debugPath: " + debugPath, Toast.LENGTH_LONG).show();
+            } else {
+
+                grantUriPermission("com.marksoft.stringart", Uri.fromFile(file), Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("image/png");
+
+                //icon.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                Log.d("DrawingView", "URI: " + Uri.fromFile(debugFile));
+
+                share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(debugFile));
+
+                startActivity(Intent.createChooser(share, "ShareActivity Image"));
+
+                //Toast.makeText(context, "startActivity finished", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getRandomFileName() {
+        // Generating a random number to save as image name
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        return "Image_" + timeStamp + "_" + n + ".png";
+    }
+
+    private void requestPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        8);
+//                        new String[]{Manifest.permission.READ_CONTACTS},
+//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/marksoft/stringart/ShareActivity.java
+++ app/src/main/java/com/marksoft/stringart/ShareActivity.java
@@ -1,10 +1,16 @@
 package com.marksoft.stringart;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,15 +22,54 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
+import fr.tvbarthel.intentshare.IntentShare;
+
 /**
  * Created by e62032 on 6/8/2016.
  */
 public class ShareActivity extends AppCompatActivity {
 
-    public void share(final Context context, final DrawingView drawingView) {
-        File savedFile = save(context, drawingView);
-        scanForNewFiles(savedFile, context);
-        //shareIntent(savedFile, context);
+    public static void share(final Activity context, final DrawingView drawingView) {
+        //File savedFile = save(context, drawingView);
+
+
+
+        drawingView.setDrawingCacheEnabled(true);
+        drawingView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = drawingView.getDrawingCache();
+
+        getPermission(context);
+        hasPermission(context);
+
+        String url = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap,
+                "StringArtImage",
+                "The image captured by MediaStore from the canvas of StringArt.");
+
+        context.grantUriPermission("com.marksoft.stringart", Uri.parse(url), Intent.FLAG_GRANT_READ_URI_PERMISSION); //TODO don't need these
+        context.grantUriPermission("com.marksoft.stringart", Uri.parse(url), Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        context.grantUriPermission("com.marksoft.stringart", Uri.parse(url), Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+        context.grantUriPermission("com.marksoft.stringart", Uri.parse(url), Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+
+        Intent intent = ShareIntent.getImageIntent(Uri.parse(url));
+        context.startActivity(intent);
+    }
+
+    private static void getPermission(Activity activity) {
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PackageManager.PERMISSION_GRANTED);
+    }
+
+    public static boolean hasPermission(Activity activity) {
+        // Assume thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == 8) {
+            Toast.makeText(activity, "PERMISSION GRANTED", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        Toast.makeText(activity, "PERMISSION NOT GRANTED was: " + ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE), Toast.LENGTH_LONG).show();
+        return false;
     }
 
     private File save(final Context context, final DrawingView drawingView) {
@@ -111,6 +156,9 @@ public class ShareActivity extends AppCompatActivity {
         }
     }
     private Intent shareIntentOld(File file, Context context){
+
+
+
 
         Log.i("ShareActivity","getDefaultShareIntent starting");
         Uri screenshotUri = Uri.fromFile(file);
